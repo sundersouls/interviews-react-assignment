f6e0ecb019d6db27b54e846f79b703787abd6a6e
sundersouls <k22570@list.ru>
2025-12-09 23:47:13 +0500

    Challenge 3 half done but i will try speedrun others if i am allowed and will be able to

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef, useCallback, memo } from "react";
 import {
   Box,
   Card,
@@ -189,6 +189,86 @@ export const Products = ({
       }
     });
   }
+
+  const ProductCard = memo(
+    ({
+      product,
+      onAddToCart,
+    }: {
+      product: Product;
+      onAddToCart: (id: number, qty: number) => void;
+    }) => {
+      // with this its rerender is seems to be optimal. idk how i will do virtual list tho i dont have much time left. ig i will try to speedrun it by ai tho i am not sure how to properly to implement it. Also coud done 4 challenge it is seems to be easier. But again i have no tiime. I wasnt in right place to do it in meantime while i had some time.
+      return (
+        <Grid key={product.id} item xs={4}>
+          {/* Do not remove this */}
+          <HeavyComponent />
+          <Card style={{ width: "100%" }}>
+            <CardMedia component="img" height="150" image={product.imageUrl} />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                {product.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Typography variant="h6" component="div">
+                ${product.price}
+              </Typography>
+              <Box flexGrow={1} />
+              <Box
+                position="relative"
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+              >
+                <Box
+                  position="absolute"
+                  left={0}
+                  right={0}
+                  top={0}
+                  bottom={0}
+                  textAlign="center"
+                >
+                  {product.loading && <CircularProgress size={20} />}
+                </Box>
+                <IconButton
+                  disabled={product.loading}
+                  aria-label="delete"
+                  size="small"
+                  onClick={() => onAddToCart(product.id, -1)}
+                >
+                  <RemoveIcon fontSize="small" />
+                </IconButton>
+
+                <Typography variant="body1" component="div" mx={1}>
+                  {product.itemInCart || 0}
+                </Typography>
+
+                <IconButton
+                  disabled={product.loading}
+                  aria-label="add"
+                  size="small"
+                  onClick={() => onAddToCart(product.id, 1)}
+                >
+                  <AddIcon fontSize="small" />
+                </IconButton>
+              </Box>
+            </CardActions>
+          </Card>
+        </Grid>
+      );
+    },
+    (prevProps, nextProps) => {
+      return (
+        prevProps.product.id === nextProps.product.id &&
+        prevProps.product.itemInCart === nextProps.product.itemInCart &&
+        prevProps.product.loading === nextProps.product.loading
+      );
+    },
+  );
 
   return (
     <Box
@@ -394,69 +474,11 @@ export const Products = ({
             </Grid>
           ) : (
             products.map((product) => (
-              <Grid key={product.id} item xs={4}>
-                {/* Do not remove this */}
-                <HeavyComponent />
-                <Card style={{ width: "100%" }}>
-                  <CardMedia
-                    component="img"
-                    height="150"
-                    image={product.imageUrl}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {product.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Typography variant="h6" component="div">
-                      ${product.price}
-                    </Typography>
-                    <Box flexGrow={1} />
-                    <Box
-                      position="relative"
-                      display="flex"
-                      flexDirection="row"
-                      alignItems="center"
-                    >
-                      <Box
-                        position="absolute"
-                        left={0}
-                        right={0}
-                        top={0}
-                        bottom={0}
-                        textAlign="center"
-                      >
-                        {product.loading && <CircularProgress size={20} />}
-                      </Box>
-                      <IconButton
-                        disabled={product.loading}
-                        aria-label="delete"
-                        size="small"
-                        onClick={() => addToCart(product.id, -1)}
-                      >
-                        <RemoveIcon fontSize="small" />
-                      </IconButton>
-
-                      <Typography variant="body1" component="div" mx={1}>
-                        {product.itemInCart || 0}
-                      </Typography>
-
-                      <IconButton
-                        disabled={product.loading}
-                        aria-label="add"
-                        size="small"
-                        onClick={() => addToCart(product.id, 1)}
-                      >
-                        <AddIcon fontSize="small" />
-                      </IconButton>
-                    </Box>
-                  </CardActions>
-                </Card>
-              </Grid>
+              <ProductCard
+                key={product.id}
+                product={product}
+                onAddToCart={addToCart}
+              />
             ))
           )}
 
